f5294c3595cc69d0366f2b932208dd2d1f1e616d
Bhagyashri <Jangam>
2013-07-30 16:53:29 +0530

    cleanup

--- charts/a6.php
+++ charts/a6.php
@@ -77,12 +77,11 @@ $js_data_string_amounts = implode($chart_violation_amount_rows, ",");
                 },
                 labels: {
                     formatter: function() {
-                       // return '$' + Highcharts.numberFormat(this.value / 1000, 0) + 'k ';
+                    
                         return '$' + Highcharts.numberFormat(this.value / 1, 0) ;                
     }
                 },
-               // minTickInterval: 5000,
-               // minorTickInterval: 5000,
+            
             },
             tooltip: {
                 formatter: function() {
