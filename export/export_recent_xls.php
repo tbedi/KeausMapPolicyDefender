080310545d99aabb7436b48c6b7b2bf1dc9a77d1
Bhagyashri <Jangam>
2013-08-01 14:53:11 +0530

    calender same date issue fixed

--- export/export_recent_xls.php
+++ export/export_recent_xls.php
@@ -4,55 +4,40 @@
 $violators_array=$_SESSION['recentArray'];
 if(isset($_SESSION['recentArray']))
 {
-       print_r($violators_array);
+       //print_r($violators_array);
 }
-
- // $limitex = $_REQUEST['limit'];
-
-
-
 
 	header('Content-Type: application/vnd.ms-excel');	//define header info for browser
 	header('Content-Disposition: attachment; filename='."Recent_Violations".'-'.date('d-m-y'));
 	header('Pragma: no-cache');
 	header('Expires: 0');
 
-	echo '<table><tr>';
+	echo '<table border=1><tr>';
 	echo '<td>SKU </td>'  ;
        echo '<td>Seller </td>';    
         echo '<td>Vendor Price </td>';    
         echo '<td>Map Price </td>';    
         echo '<td>Violation Amount </td>';   
-        
-        
-        
+                      
 	print('</tr>');
 
-	foreach ($violators_array as $violators_array ) {
-	{
-		//set_time_limit(60); // you can enable this if you have lot of data
-		$output = '<tr>';
-		for($j=0; $j< count($violators_array); $j++)
-		{
-			if(!isset($violators_array[$j]))
-				$output .= '<td>&nbsp;</td>';
-			else
-				//$output .= "<td>$violators_array[$j]</td>";
-                        $output .= "<td>".$violators_array[$j]->sku."</td>";
-                          $output .= "<td>".$violators_array[$j]->name."</td>";
-                            $output .= "<td>".$violators_array[$j]->vendor_price."</td>";
-                              $output .= "<td>".$violators_array[$j]->map_price."</td>";
-                                $output .= "<td>".$violators_array[$j]->violation_amount."</td>";
-                                
-                        
-                        
-                        
-                     
-                        
-                        
-		}
-		print(trim($output))."</tr>\t\n";
-	}
-	echo('</table>');
-        }
+        foreach ($violators_array as $violators_array ){
+   
+	 
+	$output = "<tr>";
+
+          $output .=  "<td>" . $violators_array->sku . "</td>";
+          $output .=  "<td>". $violators_array->name ."</td>";
+          $output .=  "<td>". "$ ".  $violators_array->vendor_price ."</td>";
+          $output .=  "<td>". "$ ".  $violators_array->map_price ."</td>";
+          $output .=  "<td>". "$ ".$violators_array->violation_amount ."</td>";   
+            
+    
+           print(trim($output))."</tr>\t\n";
+	  
+
+}
+       
+	echo "</table>";
+        
 ?>
--- history.php
+++ history.php
@@ -3,17 +3,17 @@
 $tableName = "crawl_results";
 $targetpage = "index.php";
 $limit = 15;
-$flagfrom=0;
-$flagto=0;
+$flagfrom = 0;
+$flagto = 0;
 
 //$_SESSION['limit'] = $limit;
 if (isset($_GET['limit']) && isset($_GET['tab']) && $_GET['tab'] == 'violations-history') {
-	$limit=$_GET['limit'];
-}  
+    $limit = $_GET['limit'];
+}
 
-static $to ;
-static $from;
-/*where*/
+$to;
+$from;
+/* where */
 
 $where = "";
 if (isset($_GET['action']) && $_GET['action'] == 'search' && isset($_GET['value']) && isset($_GET['tab']) && $_GET['tab'] == 'violations-history') {
@@ -21,23 +21,23 @@ if (isset($_GET['action']) && $_GET['action'] == 'search' && isset($_GET['value'
     $value = strtolower($_GET['value']);
     $where = "  AND  catalog_product_flat_1." . $field . "  LIKE '%" . $value . "%'";
 }
-/*where*/
+/* where */
 
 
 /* sorting */
-if ( isset($_GET['sort']) && isset($_GET['dir']) &&  isset($_GET['grid']) && $_GET['grid']=="history"  ) {  
-	$direction =$_GET['dir'];
-	$order_field =$_GET['sort'];
-	$_SESSION['sort_history_dir']=$_GET['dir'];
-	$_SESSION['sort_history_field']=$_GET['sort'];
-} else if (isset($_SESSION['sort_history_field']) && isset($_SESSION['sort_history_dir']) ) {
-	$direction = $_SESSION['sort_history_dir'];
-	$order_field =$_SESSION['sort_history_field'];
+if (isset($_GET['sort']) && isset($_GET['dir']) && isset($_GET['grid']) && $_GET['grid'] == "history") {
+    $direction = $_GET['dir'];
+    $order_field = $_GET['sort'];
+    $_SESSION['sort_history_dir'] = $_GET['dir'];
+    $_SESSION['sort_history_field'] = $_GET['sort'];
+} else if (isset($_SESSION['sort_history_field']) && isset($_SESSION['sort_history_dir'])) {
+    $direction = $_SESSION['sort_history_dir'];
+    $order_field = $_SESSION['sort_history_field'];
 } else {
-	$direction = "desc";
-	$order_field = "violation_amount";
-	$_SESSION['sort_history_dir'] = "desc";
-	$_SESSION['sort_history_field'] = "violation_amount";
+    $direction = "desc";
+    $order_field = "violation_amount";
+    $_SESSION['sort_history_dir'] = "desc";
+    $_SESSION['sort_history_field'] = "violation_amount";
 }
 $order_by = "order by " . $order_field . " " . $direction . " ";
 /* sorting */
@@ -53,35 +53,28 @@ if (isset($_GET['page']) && isset($_GET['tab']) && $_GET['tab'] == 'violations-h
 /* Pagination */
 
 /* calender dates */
- 
-if ( isset($_POST["to"]) && ($_POST["from"]) && isset($_GET['option']) && $_GET['option'] == 'show_dates' ) {  
-	$to =$_POST["to"];
-	$from =$_POST["from"];
-	$_SESSION['t']=$_POST["to"];
-	$_SESSION['fr']=$_POST["from"];
-} else if (isset($_SESSION['t']) && isset($_SESSION['fr']) ) {
-	$to = $_SESSION['t'];
-	$from =$_SESSION['fr'];
-} else {
-	 $to= date("Y-m-d");
+
+if (isset($_POST["to"]) && ($_POST["from"]) && isset($_GET['option']) && $_GET['option'] == 'show_dates') {
+    $to = $_POST["to"];
+    $from = $_POST["from"];
+    $_SESSION['t'] = $_POST["to"];
+    $_SESSION['fr'] = $_POST["from"];
+}
+// if (isset($_SESSION['t']) && isset($_SESSION['fr']) ) {
+//	$to = $_SESSION['t'];
+//	$from =$_SESSION['fr'];
+//} 
+else {
+    $to= date("Y-m-d");
          $from= date('Y-m-d',strtotime("-1 days"));
 	$_SESSION['t'] = date("Y-m-d");
 	$_SESSION['fr'] = date('Y-m-d',strtotime("-1 days"));
-}        
+}
 /* calender dates */
 
 
 
-
-                   if( $to==NULL or $from==NULL or $from > $to )
-                       {
-                         echo '<script type="text/javascript">alert("Enter correct dates!");</script>';
-                       }
-
-                if( $to == $from) 
-{
-    $from=$to;
-    $sql="SELECT SQL_CALC_FOUND_ROWS  
+    $sql = "SELECT SQL_CALC_FOUND_ROWS  
     catalog_product_flat_1.sku as sku,
     date_format(crawl.date_executed,'%d-%m-%Y %H:%i:%s') as date_executed,
 catalog_product_flat_1.name as pname,
@@ -98,64 +91,38 @@ inner join catalog_product_flat_1
 on catalog_product_flat_1.entity_id=crawl_results.product_id
 inner join crawl
 on crawl.id=crawl_results.crawl_id
-where date_format(date_executed, '%Y-%m-%d' )='$from'   
+where (date_format(crawl.date_executed,'%Y-%m-%d') between '$from' and '$to' )  
 and
-crawl_results.violation_amount>0.05 ".$where." 
-and website.excluded=0 
-" . $order_by . " LIMIT $start, $limit "; 
-   
-}
-
-
-else
-{
-$sql="SELECT SQL_CALC_FOUND_ROWS  
-    catalog_product_flat_1.sku as sku,
-    date_format(crawl.date_executed,'%d-%m-%Y %H:%i:%s') as date_executed,
-catalog_product_flat_1.name as pname,
-website.name as wname, 
-cast(crawl_results.vendor_price as decimal(10,2)) as vendor_price,
-cast(crawl_results.map_price as decimal(10,2)) as map_price,
-cast(crawl_results.violation_amount as decimal(10,2)) as violation_amount,
-crawl_results.website_product_url
-from website
-inner join
-crawl_results
-on website.id = crawl_results.website_id
-inner join catalog_product_flat_1
-on catalog_product_flat_1.entity_id=crawl_results.product_id
-inner join crawl
-on crawl.id=crawl_results.crawl_id
-where  (crawl.date_executed between '$from' and '$to' )  
-and
-crawl_results.violation_amount>0.05 ".$where." 
+crawl_results.violation_amount>0.05 " . $where . " 
 and website.excluded=0 
 " . $order_by . " LIMIT $start, $limit ";
+
+
+
+$violators_array = $db_resource->GetResultObj($sql);
+
+
+
+
+
+$_SESSION['historyArray'] = $violators_array;
+if (isset($_SESSION['historyArray'])) {
+    // print_r($_SESSION['historyArray']); 
 }
-
-
-$violators_array=$db_resource->GetResultObj($sql);
-
-$_SESSION['historyArray']=$violators_array;
-if(isset($_SESSION['historyArray']))
-{
-   // print_r($_SESSION['historyArray']); 
-}
-
 ?>
 <script type="text/javascript">
-    
-//document.getElementById(inputFieldfrom).value= <?php $from ;?>
-//document.getElementById(inputFieldto).value= <?php $to ;?>
-document.getElementById(inputFieldfrom).setAttribute(value, <?php $from ; ?> );
-//document.getElementById(inputFieldto).setAttribute(value, <?php $from ; ?> );
+
+//document.getElementById(inputFieldfrom).value= <?php $from; ?>
+//document.getElementById(inputFieldto).value= <?php $to; ?>
+    document.getElementById(inputFieldfrom).setAttribute(value, <?php $from; ?>);
+//document.getElementById(inputFieldto).setAttribute(value, <?php $from; ?> );
 
 </script>
-    <?php
+<?php
 // Initial page num setup
-$sql1=" SELECT FOUND_ROWS() as total;";
-$total_pages=$db_resource->GetResultObj($sql1);
-$total_pages=$total_pages[0]->total;
+$sql1 = " SELECT FOUND_ROWS() as total;";
+$total_pages = $db_resource->GetResultObj($sql1);
+$total_pages = $total_pages[0]->total;
 
 $tab_name = 'violations-history';
 $prev = $page - 1;
@@ -164,18 +131,15 @@ $lastpage = ceil($total_pages / $limit);
 $LastPagem1 = $lastpage - 1;
 
 $page_param = "page"; //variable used for pagination
-$additional_params = "&limit=".$limit;  //addtiion params to pagination url;
+$additional_params = "&limit=" . $limit;  //addtiion params to pagination url;
 
 
 
 if (isset($_GET['action']) && $_GET['action']) { // search 
     $additional_params.="&action=" . $_GET['action'] . "&field=sku&value=" . $_GET['value'];
 }
- 
+
 
 include_once 'template/history_tab.phtml';
-
-
-
 ?>
 
