356305e94f258b4da0bbcf057ead9db5c0a4a287
Bhagyashri <Jangam>
2013-08-09 13:27:00 +0530

    history page alignment

--- charts/a5.php
+++ charts/a5.php
@@ -117,4 +117,4 @@ if (!$result) {
     echo mysql_error();
 }
 ?>
-<div id="chart-a5" style="min-width: 490px; height: 400px; margin: 0 auto"></div>
+<div id="chart-a5" style="min-width: 700px; height: 400px; margin: 0 auto"></div>
--- charts/a6.php
+++ charts/a6.php
@@ -123,4 +123,4 @@ if (!$result) {
     echo mysql_error();
 }
 ?>
-<div id="chart-a6" style="min-width: 510px; height: 400px; margin: 0 auto"></div>
+<div id="chart-a6" style="min-width: 700px; height: 400px; margin: 0 auto"></div>
