fc3b5c37aa7b3153dcb4ac70674cd7288c6b6a46
RPooja <pverma@kraususa.com>
2013-08-20 18:42:54 +0530

    final commit of today 20/8

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,6 @@
 <?php 
 //dashboard page
-$sql1 = "SELECT
+$sql1 = "SELECT 
 website.name name, crawl_results.website_id, count(crawl_results.website_id) countcurrent, tab1.countprev countprev, count(crawl_results.website_id)-tab1.countprev diff, count(crawl_results.website_id)+tab1.countprev countsum
 from website
 inner join
@@ -21,22 +21,31 @@ inner join crawl
 on crawl.id=crawl_results.crawl_id
 and crawl_results.crawl_id = (select max(id) from crawl where id != (select max(id) from crawl))
 group by website.name
-) as tab1 on tab1.name = website.name
+) as tab1 on tab1.name = website.name where crawl_results.violation_amount>0.05 and website.excluded=0
 group by website.name
 order by countcurrent desc limit 10
 ";
-$resultDls = mysql_query($sql1);
+$dashboard_array = $db_resource->GetResultObj($sql1);
+
 
 $sql2 = "select
-catalog_product_flat_1.sku, crawl_results.product_id, 
-count(crawl_results.product_id) as currentcount, if(tab1.prevcount is null,0,tab1.prevcount) prevcount, count(crawl_results.product_id)-if(tab1.prevcount is null,0,tab1.prevcount) diff,
+catalog_product_flat_1.sku, crawl_results.product_id,
+count(crawl_results.product_id) as currentcount, 
+if(tab1.prevcount is null,0,tab1.prevcount) prevcount, 
+count(crawl_results.product_id)-if(tab1.prevcount is null,0,tab1.prevcount) diff,
 count(crawl_results.product_id)+if(tab1.prevcount is null,0,tab1.prevcount) countsum
-from crawl_results inner join catalog_product_flat_1 on crawl_results.product_id = catalog_product_flat_1.entity_id
-inner join crawl on crawl.id = crawl_results.crawl_id and crawl_results.crawl_id = (select max(id) from crawl) left join ( select
-catalog_product_flat_1.sku sku1, count(crawl_results.product_id) as prevcount from crawl_results inner join catalog_product_flat_1 on crawl_results.product_id = catalog_product_flat_1.entity_id
-inner join crawl on crawl.id = crawl_results.crawl_id and crawl_results.crawl_id = (select max(id) from crawl where id != (select max(id) from crawl))
-group by crawl_results.product_id ) as tab1 on tab1.sku1 = catalog_product_flat_1.sku group by crawl_results.product_id order by currentcount desc limit 10";
-$resulttop = mysql_query($sql2);
+from crawl_results inner join catalog_product_flat_1 
+on crawl_results.product_id = catalog_product_flat_1.entity_id
+inner join crawl on crawl.id = crawl_results.crawl_id 
+and crawl_results.crawl_id = (select max(id) from crawl) left join ( select
+catalog_product_flat_1.sku sku1, 
+count(crawl_results.product_id) as prevcount from crawl_results 
+inner join catalog_product_flat_1 on crawl_results.product_id = catalog_product_flat_1.entity_id
+inner join crawl on crawl.id = crawl_results.crawl_id and crawl_results.crawl_id = (select max(id) 
+from crawl where id != (select max(id) from crawl))
+group by crawl_results.product_id ) as tab1 on tab1.sku1 = catalog_product_flat_1.sku 
+where crawl_results.violation_amount>0.05 group by crawl_results.product_id order by currentcount desc limit 10";
+$dash_array = $db_resource->GetResultObj($sql2);
 
 $sql3 = "SELECT
 catalog_product_flat_1.sku,
@@ -58,7 +67,7 @@ and
 crawl.id =
 (select max(crawl.id) from crawl) order by violation_amount desc
 limit 10";
-$result3 = mysql_query($sql3);
+$dash1_array = $db_resource->GetResultObj($sql3);
 
 $sql = "select 
 catalog_product_flat_1.sku
@@ -84,20 +93,18 @@ and crawl_results.crawl_id in (select id from crawl
 where date_format(date_executed, '%Y-%m-%d')
 between DATE_ADD(sysdate(), INTERVAL -3 DAY) and sysdate())
 group by crawl_results.product_id ";
-            $result = mysql_query($sql);
-            $result1 = mysql_query($sqll);
+             $dash2_array = $db_resource->GetResultObj($sql);
+             $dash3_array = $db_resource->GetResultObj($sqll);
             $array=array();
             $array2=array();
             $resultst = array();
-           while($row = mysql_fetch_array($result))
-           {
-                   
-                       array_push($array,$row['sku']);
-            };
-           while($row2 = mysql_fetch_array($result1)){
-               array_push($array2,$row2['sku']);  
-           };
-               
+            foreach ($dash2_array as $dash2) {
+             array_push($array,$dash2->sku);
+            }
+            foreach ($dash3_array as $dash3) {
+             array_push($array2,$dash3->sku);
+            }
+              
              $resultst = array_diff($array,$array2);
              
              $sql = "select 
@@ -131,19 +138,17 @@ max(id)
 from
 crawl))
 group by crawl_results.product_id ";
-            $result = mysql_query($sql);
-            $result1 = mysql_query($sqll);
+             $dash4_array = $db_resource->GetResultObj($sql);
+             $dash5_array = $db_resource->GetResultObj($sqll);
             $array=array();
             $array2=array();
             $resultstrt = array();
-           while($row = mysql_fetch_array($result))
-           {
-                   
-                       array_push($array,$row['sku']);
-            };
-           while($row2 = mysql_fetch_array($result1)){
-               array_push($array2,$row2['sku']);  
-           };
+            foreach ($dash4_array as $dash4) {
+             array_push($array,$dash4->sku);
+            }
+            foreach ($dash5_array as $dash5) {
+             array_push($array2,$dash5->sku);
+            }
                
              $resultstrt = array_diff($array,$array2);
              
@@ -168,19 +173,18 @@ from
 where date_format(date_executed, '%Y-%m-%d')
 between DATE_ADD(sysdate(), INTERVAL -3 DAY) and sysdate())
 group by website.name ";
-            $result = mysql_query($sql);
-            $result1 = mysql_query($sqll);
+              $dash6_array = $db_resource->GetResultObj($sql);
+             $dash7_array = $db_resource->GetResultObj($sqll);
+
             $array=array();
             $array2=array();
             $resultstv = array();
-           while($row = mysql_fetch_array($result))
-           {
-                   
-                       array_push($array,$row['name']);
-            };
-           while($row2 = mysql_fetch_array($result1)){
-               array_push($array2,$row2['name']);  
-           };
+            foreach ($dash6_array as $dash6) {
+             array_push($array,$dash6->name);
+            }
+            foreach ($dash7_array as $dash7) {
+             array_push($array2,$dash7->name);
+            }
                
              $resultstv = array_diff($array,$array2);
              
@@ -211,22 +215,19 @@ from
             crawl
 where id !=(select max(id) from crawl))
 group by website.name";
-            
-            $result = mysql_query($sql);
-            $result1 = mysql_query($sqll);
+            $dash8_array = $db_resource->GetResultObj($sql);
+             $dash9_array = $db_resource->GetResultObj($sqll);
+
             $array=array();
             $array2=array();
             $resultstrtv = array();
-           while($row = mysql_fetch_array($result))
-           {
-                   
-                       array_push($array,$row['name']);
-            };
-           while($row2 = mysql_fetch_array($result1)){
-               
-               array_push($array2,$row2['name']);
-               
-           };
+            foreach ($dash8_array as $dash8) {
+             array_push($array,$dash8->name);
+            }
+            foreach ($dash9_array as $dash9) {
+             array_push($array2,$dash9->name);
+            }
+
                
              $resultstrtv = array_diff($array,$array2);
 
