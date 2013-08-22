2dd7d02bdbf41fc341a1a54673ff8e873ff10e2b
Bhagyashri <Jangam>
2013-08-22 18:23:34 +0530

    Removed recent tab and resp codes

--- index.php
+++ index.php
@@ -26,22 +26,13 @@ $pagination= new Pagination();
                     <div id="tabs" align="center">
                         <ul>
                         	<li id="tabHeader_1" class="dashboard">Dashboard</li>
-                            <li id="tabHeader_2" class="recent">Recent violations</li>
+                           
                             <li id="tabHeader_5" class="violations-history">Violation history</li>
                         </ul>
                     </div>
                     <div id="tabscontent" align="center">
                         <div class="tabpage dashboard" id="tabpage_1">
                             <?php include_once 'dashboard.php'; ?>
-                        </div>
-                        <div class="tabpage recent" id="tabpage_2">
-                            <?php include_once 'recent.php'; ?>
-                        </div>
-                        <div class="tabpage violation-by-product" id="tabpage_3">
-                            <?php include_once 'product.php'; ?>
-                        </div>
-                    	<div class="tabpage violation-by-seller" id="tabpage_4">
-                            <?php include_once 'vendor.php'; ?>
                         </div>
                         <div class="tabpage violations-history" id="tabpage_5">
                             <?php include_once 'history.php'; ?>
